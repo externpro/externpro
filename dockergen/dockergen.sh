5af0b5eae97326ccc2eac2f71ef6d63d3dd0e69c
Scott M Anderson <smanders@sdl.usu.edu>
2024-11-14 17:55:35 -0700

    rocky85-pin.dockerfile
    issue https://github.com/externpro/buildpro/issues/93

--- dockergen/dockergen.sh
+++ dockergen/dockergen.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
-for img in rocky85-pro rocky85-bld
+for img in rocky85-pro rocky85-bld rocky85-pin
 do
   dfile=../${img}.dockerfile
   awk -v r="${img}" '{gsub(/%BP_REPO%/,r)} 1' bit.head.dockerfile > ${dfile}
-  if [[ ${img} == *"-bld"* ]]; then
-    cat bit.isrhub.dockerfile >> ${dfile}
+  if [[ ${img} == *"-pro"* ]]; then
     cat bit.user.dockerfile >> ${dfile}
   else
+    cat bit.isrhub.dockerfile >> ${dfile}
     cat bit.user.dockerfile >> ${dfile}
   fi
   cat bit.tail.dockerfile >> ${dfile}
