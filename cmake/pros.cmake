1a6133e6252557abc77e0fea10253229a896293f
Scott M Anderson <smanders@sdl.usu.edu>
2025-05-02 17:38:11 -0600

    pros: nvjpeg2000 v0.8.0.1 from github/externpro
    * issue https://github.com/externpro/externpro/issues/105
    * release https://github.com/externpro/nvJPEG2000/releases/tag/v0.8.0.1

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -154,6 +154,14 @@ set(xp_nasm REPO github.com/externpro/nasm TAG v2.14.02
   SHA256_Linux 31fb78aa856e58716b5cf36927a24824e1fc931a375b02b9c24245a5ed3e3347
   SHA256_win64 ddf6097be3ecf6e63cdcc56dbc6f063f44cf4ba04e8df73c2c6c3798c3f98428
   )
+set(xp_nvjpeg2000 REPO github.com/externpro/nvJPEG2000 TAG v0.8.0.1
+  BRANCH dev
+  WEB "https://developer.nvidia.com/nvjpeg"
+  DESC "high-performance GPU-accelerated library for decoding JPEG 2000 format images"
+  LICENSE "[NVIDIA](https://docs.nvidia.com/cuda/nvjpeg2000/license.html 'NVIDIA Software License Agreement')"
+  SHA256_Linux 8ee32edd1474bc7173c03085da2ee8372a73324ba13ab1941d853a07362de92a
+  SHA256_win64 23258bd1043a4cb290439eb7b01fbec3af001e806cef4c8c75186f57bf1598d4
+  )
 set(xp_patch REPO github.com/externpro/patch TAG v2.7
   BRANCH main
   WEB "https://savannah.gnu.org/projects/patch/" UPSTREAM "git.savannah.gnu.org/cgit/patch.git"
