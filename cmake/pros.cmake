5fcc842c1857991796b5975cf22c5780827c598d
Scott M Anderson <smanders@sdl.usu.edu>
2025-05-07 17:08:20 -0600

    pros: nvjpeg2000 v0.8.1.1 from github/externpro
    * resolves https://github.com/externpro/externpro/issues/105
    * release https://github.com/externpro/nvJPEG2000/releases/tag/v0.8.1.1

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -154,14 +154,14 @@ set(xp_nasm REPO github.com/externpro/nasm TAG v2.14.02
   SHA256_Linux 31fb78aa856e58716b5cf36927a24824e1fc931a375b02b9c24245a5ed3e3347
   SHA256_win64 ddf6097be3ecf6e63cdcc56dbc6f063f44cf4ba04e8df73c2c6c3798c3f98428
   )
-set(xp_nvjpeg2000 REPO github.com/externpro/nvJPEG2000 TAG v0.8.0.2
+set(xp_nvjpeg2000 REPO github.com/externpro/nvJPEG2000 TAG v0.8.1.1
   BRANCH dev
   WEB "https://developer.nvidia.com/nvjpeg"
   DESC "high-performance GPU-accelerated library for decoding JPEG 2000 format images"
   LICENSE "[NVIDIA](https://docs.nvidia.com/cuda/nvjpeg2000/license.html 'NVIDIA Software License Agreement')"
-  SHA256_JetPack 064099ea2c3f276a1b1ef5177c378148a2dd2344fc6ccfda1a4ceed6dcca594c
-  SHA256_Linux d172b061b29d1a1aa5ec2f83b085c1549dea739b05f2d0acf85874d8fecc2c9e
-  SHA256_win64 0d676dfe3de353949af5fe60f7e799d274bc2acae512e78de3ef2de4a81547a4
+  SHA256_JetPack 9933d9515c2782a7371cb48eec85efd1bf209a6de6cff5fde41d378bfa108453
+  SHA256_Linux aae48f9e42b5271335d630e064bef89d97a4a0723b468b81877e35cf9f5be525
+  SHA256_win64 e08dd32554869c1cfd6add59d9a7cec4c00cd9fc74ada137fdd1b5d236711431
   )
 set(xp_patch REPO github.com/externpro/patch TAG v2.7
   BRANCH main
