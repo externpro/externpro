fca21c51c9644af0493a3d9db69a5aab17ac2fcf
Scott M Anderson <anderson.scott.m@icloud.com>
2025-08-15 13:33:05 -0600

    pros: fmt v11.2.0.1 from github/externpro
    * resolves https://github.com/externpro/externpro/issues/154
    * release https://github.com/externpro/fmt/releases/tag/v11.2.0.1

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -48,6 +48,15 @@ set(xp_flatbuffers REPO github.com/externpro/flatbuffers VER 2.0.6 XP_MODULE
   DESC "efficient cross platform serialization library"
   LICENSE "[Apache-2.0](https://github.com/google/flatbuffers/blob/v2.0.6/LICENSE.txt 'Apache License, Version 2.0')"
   )
+set(xp_fmt REPO github.com/externpro/fmt TAG v11.2.0.1
+  BASE 11.2.0 BRANCH dev
+  WEB "https://fmt.dev" UPSTREAM "github.com/fmtlib/fmt"
+  DESC "fmtlib: a modern formatting library"
+  LICENSE "[MIT](https://github.com/externpro/fmt/blob/master/LICENSE 'MIT License')"
+  SHA256_Linux-arm64 e40875ae284d49555a736feef7e8541a2499ef26e8a23cc73c1cf3e34aea7a84
+  SHA256_Linux 8cced6ff19d1c179c329cac9b4670e345a1c691a2f78f5478141a0211c261b13
+  SHA256_win64 4ecc92aa3d83b580d62b200c20ca3d2d749d63c5091916a4462a14ab4c506b38
+  )
 set(xp_geos REPO github.com/externpro/geos TAG v3.13.0.3
   BASE 3.13.0 BRANCH dev
   WEB "https://libgeos.org" UPSTREAM "github.com/libgeos/geos"
