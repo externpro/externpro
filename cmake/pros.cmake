b0bb5a6334a2c212b1b6b0c6aced677120641076
Scott M Anderson <anderson.scott.m@icloud.com>
2025-09-22 12:17:08 -0600

    pros: libzmq v4.3.4.1 from github/externpro
    * issue https://github.com/externpro/externpro/issues/174
    * release https://github.com/externpro/libzmq/releases/tag/v4.3.4.1

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -369,11 +369,15 @@ set(xp_libspatialite REPO github.com/externpro/libspatialite TAG v5.1.0.6
   SHA256_Linux 00549a7292e6f0391d585c1de642b142d0cb36f67bd01391fc0f89f6c622c506
   SHA256_win64 cb744298b24539e1cad2cb17db9909bbc9ee209bf34c018049b7a42ab767f832
   )
-set(xp_libzmq REPO github.com/externpro/libzmq VER 4.3.4 XP_MODULE
-  BASE zeromq:v4.3.4 BRANCH xp4.3.4 DEPS sodium
+set(xp_libzmq REPO github.com/externpro/libzmq TAG v4.3.4.1
+  BASE v4.3.4 BRANCH dev DEPS sodium
   WEB "https://zeromq.org/" UPSTREAM "github.com/zeromq/libzmq"
   DESC "high-performance asynchronous messaging library"
   LICENSE "[MPL-2.0](http://wiki.zeromq.org/area:licensing 'Mozilla Public License 2.0')"
+  SHA256_Darwin-arm64 377183d82a6f233430f8b9e49e2a060249ff1cd85316b1902474823231e003a0
+  SHA256_Linux-arm64 4ae9c31dc434e59cbab1d9d5dddaa890be07d8447d16b948ab0e1ca97043f504
+  SHA256_Linux 04b8fe7318f8a65e8f1811617ace8fb065663d34b0dc26433f183119261a8f40
+  SHA256_win64 1da8ae3cb8eef5a355cbf8d5c5178ee9ee9d1dc8b8a90c55c1fd12674f77b03d
   )
 set(xp_node-addon-api REPO github.com/externpro/node-addon-api TAG v8.5.0.1
   BASE v8.5.0 BRANCH dev DEPS nodexp
