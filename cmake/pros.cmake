e2d1afd2d537f7eb2166527ff6239f4bcee6b44b
Scott M Anderson <anderson.scott.m@icloud.com>
2025-10-12 00:35:39 -0600

    pros: geotranz v2.4.2.1 from github/externpro
    * issue https://github.com/externpro/externpro/issues/199
    * release https://github.com/externpro/geotranz/releases/tag/v2.4.2.1

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -100,11 +100,15 @@ set(xp_geos REPO github.com/externpro/geos TAG v3.13.0.4
   SHA256_Linux 2853045a06fd25535bbd157cea19bf0d9073882c58f179b0eb790544d3ba9a34
   SHA256_win64 bb776f18b782d4287db2b81dbd85831776836a06c4da344aaac7dbbffedea740
   )
-set(xp_geotrans REPO github.com/externpro/geotranz VER 2.4.2 XP_MODULE
-  BASE v2.4.2 BRANCH xp2.4.2
+set(xp_geotranz REPO github.com/externpro/geotranz TAG v2.4.2.1
+  BASE v2.4.2 BRANCH dev
   WEB "https://earth-info.nga.mil"
   DESC "geographic translator (convert coordinates)"
   LICENSE "[public domain](https://github.com/externpro/geotranz 'see GEOTRANS Terms of Use in README or download https://earth-info.nga.mil/php/download.php?file=wgs-terms')"
+  SHA256_Darwin-arm64 35bc276cafb52bacf1b232c36a990e709a1a6bebad066c4a8e657b4026149af5
+  SHA256_Linux-arm64 52afe07b245405d7c7ec093c68377cf2eec636268ca40b7b536536b212aec688
+  SHA256_Linux 22aa67e65c8c9034ae060fd34a09238328bfc9a55d1bfd4b6d4d6b0bef03fb2e
+  SHA256_win64 687cee720040645ba293d51c2c8d1353fd256fe9e7058198324493f9dbc7db2b
   )
 set(xp_glew REPO github.com/externpro/glew VER 1.13.0 XP_MODULE
   BASE nigels-com:glew-1.13.0 BRANCH xp-1.13.0
