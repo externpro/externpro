0cc92f149894be4cde929d31a47b27aa150a24ae
Scott M Anderson <anderson.scott.m@icloud.com>
2025-11-18 19:11:41 -0700

    pros: jpegxp v6.25.1 from github/externpro
    * issue https://github.com/externpro/externpro/issues/192
    * release https://github.com/externpro/jpegxp/releases/tag/v6.25.1

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -237,11 +237,15 @@ set(xp_jasper REPO github.com/externpro/jasper TAG v1.900.1.1
   SHA256_Linux 0f6ac4aade72a0d0cbecbd5cdd4113486051edfc99516927fd0f33e632a68378
   SHA256_win64 a3c516004b010fd280c342e1c3f42bed9b1752b6b47bb3882d40d62315a2c56b
   )
-set(xp_jpegxp REPO github.com/externpro/jpegxp VER 24.01 XP_MODULE
-  XPBLD "intro" BASE jxp.240125 BRANCH jxp
+set(xp_jpegxp REPO github.com/externpro/jpegxp TAG v6.25.1
+  XPBLD "intro" BASE jxp.240125 BRANCH dev
   WEB "http://www.ijg.org/"
   DESC "JPEG codec with mods for Lossless, 12-bit lossy (XP)"
   LICENSE "[IJG](https://github.com/externpro/libjpeg/blob/upstream/README 'Independent JPEG Group License, see LEGAL ISSUES in README')"
+  SHA256_Linux 13f5b648a9d7cf63017521541241a8476da27f0197f3952c28904aa45975df60
+  SHA256_Linux-arm64 48cc74c37ade055d45a2b8b6b0e5e73a8f58222ae88565956272811c5519077a
+  SHA256_win64 2a18e83a8f670a1dfaa89f821f31e8886adb82e8627c2e68c029f9d17cb0013b
+  SHA256_Darwin-arm64 88f6659543c0ab6a88c48007262e35a6253aa245dacd33c44dcbfa2b99a2a93c
   )
 set(xp_jpeglossless REPO github.com/externpro/libjpeg VER 62.1
   BASE eccc424 BRANCH lossless.6b
