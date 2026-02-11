f41d393ce8f00d18239fa6e61a0034008c1d6482
Scott M Anderson <anderson.scott.m@icloud.com>
2026-02-11 16:27:48 -0700

    pros: nodeng xpv24.13.0.2 from github/externpro
    * release https://github.com/externpro/nodeng/releases/tag/xpv24.13.0.2

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -131,8 +131,8 @@ set(xp_nodeng REPO github.com/externpro/nodeng TAG v22.19.0.2-2-g3a430c8
   MANIFEST_SHA256 60f9d99fb469a6f013924b938be607f6258b8337711eeb0b93e8d1ff1ee28772
   )
 # last xp_nodeng set is used
-set(xp_nodeng REPO github.com/externpro/nodeng TAG v24.13.0.1
-  MANIFEST_SHA256 56951d397ed17096618efa3fc3a0916663f388a1a47e9a1dc7ac9d1373ff761d
+set(xp_nodeng REPO github.com/externpro/nodeng TAG xpv24.13.0.2
+  MANIFEST_SHA256 d69bd8e242d199aee331701c4b5681712b0b9bdcc76f8a83ba5262a98226d9eb
   )
 # TRICKY: nodexp, openssl versions coordinated
 set(xp_nodexp REPO github.com/externpro/nodexp TAG xpv22.19.0.3
