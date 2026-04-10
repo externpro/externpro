79e6f0d0d98f293db42864044446d524596fffd0
Scott M Anderson <anderson.scott.m@icloud.com>
2026-04-10 10:12:57 -0600

    pros: Threads xpv1.0.1 from github/externpro
    * release https://github.com/externpro/Threads/releases/tag/xpv1.0.1
    * issue https://github.com/externpro/externpro/issues/319

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -172,8 +172,8 @@ set(xp_spdlog REPO github.com/externpro/spdlog TAG xpv1.17.0.1
 set(xp_SQLite3 REPO github.com/externpro/SQLite3 TAG xpv3.38.2.3
   MANIFEST_SHA256 a41857465c8b760e7f500f0115bdd83d1151da6f4cefabe9c0baf2ae52cb13a6
   )
-set(xp_threads REPO github.com/externpro/Threads TAG xpv1.0.0
-  MANIFEST_HASH SHA256=99112c83332fbe560f6b375e310388a56ac014d0ff3ebacdf691a7b724bc000d
+set(xp_Threads REPO github.com/externpro/Threads TAG xpv1.0.1
+  MANIFEST_HASH SHA256=d5394ae19abc5d8700d9390985afb301549173a55b8d1182aa675cd1a4a06ee5
   )
 set(xp_wirehair REPO github.com/externpro/wirehair TAG xpv23.12.02.3
   MANIFEST_SHA256 8d0617549ee6e5d07df73d2336d78e46ee34ddc0d3f6660fdfccfee3b5238266
