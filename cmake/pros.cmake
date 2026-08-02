8234f4e30b018ca3d63a4e2c7892105a11fec3bc
Scott M Anderson <anderson.scott.m@icloud.com>
2026-08-01 18:32:40 -0600

    pros: eigen xpv3.4.0.4 from github/externpro
    * release https://github.com/externpro/eigen/releases/tag/xpv3.4.0.4

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -35,8 +35,8 @@ set(xp_curl REPO github.com/externpro/curl TAG xpv7.80.0.4
   MANIFEST_SHA256 c3e620e11af6f71600d18dbe0910fae4c94943a532965b160b4872a360ad1cd9
   )
 set(ap_CURL curl)
-set(xp_eigen REPO github.com/externpro/eigen TAG xpv3.4.0.3
-  MANIFEST_SHA256 63af710c14984eab4e99ab7909e9d130735a6e5635316e6b900a1206ab179a32
+set(xp_eigen REPO github.com/externpro/eigen TAG xpv3.4.0.4
+  MANIFEST_HASH SHA256=b740e0c887043d310fea3088eae824d3c382d84049c6d3e3e9bee43d889e64b9
   )
 set(ap_Eigen3 eigen)
 set(xp_fecpp REPO github.com/externpro/fecpp TAG xpv0.10.1
