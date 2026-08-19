8617efadf546a9add2fc4ae17a6781bff7b48df5
Scott M Anderson <anderson.scott.m@icloud.com>
2026-08-19 09:50:43 -0600

    pros: openssl xpv1.1.1l.5 from github/externpro
    * release https://github.com/externpro/openssl/releases/tag/xpv1.1.1l.5

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -156,8 +156,8 @@ set(xp_openh264 REPO github.com/externpro/openh264 TAG xpv1.4.0.4
   MANIFEST_HASH SHA256=9750f64da1e618ecd7801fa4cc50100c2ac80b7d1b2297b771eee1815a7a4486
   )
 # TRICKY: nodexp, openssl versions coordinated
-set(xp_openssl REPO github.com/externpro/openssl TAG xpv1.1.1l.4
-  MANIFEST_SHA256 7c65fa87cd7692947e31933ac18799ca29b10831fcefc2d31af2d22e9183886a
+set(xp_openssl REPO github.com/externpro/openssl TAG xpv1.1.1l.5
+  MANIFEST_HASH SHA256=3fc8d66a89fa5950606c794ffed9a8009f33f100edfd495b06ee65959215b42b
   )
 set(ap_OpenSSL openssl)
 set(xp_patch REPO github.com/externpro/patch TAG xpv2.7.6.7
