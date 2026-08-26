ea5e96725eada778d2b015633415612502829b09
Scott M Anderson <anderson.scott.m@icloud.com>
2026-08-26 15:00:25 -0600

    pros: libgit2 xpv1.3.0.5 from github/externpro
    * release https://github.com/externpro/libgit2/releases/tag/xpv1.3.0.5

--- cmake/pros.cmake
+++ cmake/pros.cmake
@@ -91,8 +91,8 @@ set(ap_EXPAT libexpat)
 set(xp_libgeotiff REPO github.com/externpro/libgeotiff TAG xpv1.2.4.6
   MANIFEST_HASH SHA256=d1d6b0c94fe2db5998f22da380341db766d625734320a635a98fa2d6527b8c8c
   )
-set(xp_libgit2 REPO github.com/externpro/libgit2 TAG xpv1.3.0.4
-  MANIFEST_HASH SHA256=8f44c5e3df84a74fa4089c150ae5d4abd1ecdc57f62b255f3bf874167232784e
+set(xp_libgit2 REPO github.com/externpro/libgit2 TAG xpv1.3.0.5
+  MANIFEST_HASH SHA256=1606bcd4c339b450ad1786e340a889002b083bebaf7a3f117e95c0bdbf5ca7ca
   )
 set(xp_libiconv REPO github.com/externpro/libiconv TAG xpv1.18.9
   MANIFEST_HASH SHA256=bdfa1aa2270a550772647fbac7795d5fafa7fce2b1345dd94f740dda88e05dc4
